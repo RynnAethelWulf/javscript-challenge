39a944995546d7a90cb2f2146204a651ffa597fd
AdvaliTech Pty Ltd <bimalkprabha24@gmail.com>
2020-12-14 09:49:57 +0800

    updatel the gtml and css

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,37 +1,12 @@
 // from data.js
 let tableData = data;
 
-// YOUR CODE HERE!
+// selecting form and button
 let form = d3.select("form");
 let button = d3.select("#filter-btn");
 
-// For Selection
-d3.selectAll("#selDataset").on("change", updateTable);
 
-function updateTable() {
-
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
-    // console.log(dataset);
-    // let filteredDataCountry = tableData.filter(ele => ele.country === dataset);
-    // console.log(filteredDataCountry)
-    // tbody.html("");
-    // filteredDataCountry.forEach((ele) => {
-    //     let table_row = tbody.append("tr");
-    //     Object.entries(ele).forEach(([key, value]) => {
-    //         table_row.append("td").text(value);
-    //     });
-
-
-    // });
-
-    return dataset
-
-}
-
-
-// Output
+// Output display
 let tbody = d3.select("tbody");
 
 button.on("click", filterdata);
@@ -39,15 +14,12 @@ form.on("submit", filterdata);
 
 function filterdata() {
     d3.event.preventDefault();
-    let dropDownTag;
-    updateTable() ? dropDownTag = updateTable() : dropDownTag = "datetime";
 
-    // let dropDownTag = "datetime";
 
     let inputData = d3.select("#datetime").property("value");
     // let inputData = datetime.value();
 
-    let filteredData = tableData.filter(ele => ele[dropDownTag] === inputData);
+    let filteredData = tableData.filter(ele => ele["datetime"] === inputData);
     console.log(filteredData)
     tbody.html("");
     filteredData.forEach((ele) => {
